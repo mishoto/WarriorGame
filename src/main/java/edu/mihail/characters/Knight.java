68ffcee34e0a15e7cd67c888a5b36762f733ed2e
mishoto <mihail.dev.io@gmail.com>
2022-11-12 09:24:12 +0200

    make attack constant remove health field, constructor, getHealth() and hitByWarrior()

--- src/main/java/edu/mihail/characters/Knight.java
+++ src/main/java/edu/mihail/characters/Knight.java
@@ -2,25 +2,10 @@ package edu.mihail.characters;
 
 public class Knight extends Warrior{
 
-    private int health;
-    private int attack;
-
-    public Knight() {
-        this.health = 50;
-        this.attack = 7;
-    }
-
-    @Override
-    public int getHealth() {
-        return this.health;
-    }
+    private static final int ATTACK = 7;
 
     @Override
     public int getAttack() {
-        return this.attack;
-    }
-
-    public void hitByWarrior(Warrior warrior){
-        health = this.getHealth() - warrior.getAttack();
+        return ATTACK;
     }
 }
