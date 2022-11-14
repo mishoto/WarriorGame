2464ecd9d7a0e76ade542dcbdf8f12fd5e63a0f4
mishoto <mihail.dev.io@gmail.com>
2022-11-15 00:07:53 +0200

    minor refactoring

--- src/main/java/edu/mihail/fightscene/Duel.java
+++ src/main/java/edu/mihail/fightscene/Duel.java
@@ -2,26 +2,26 @@ package edu.mihail.fightscene;
 
 import edu.mihail.characters.Knight;
 import edu.mihail.characters.Warrior;
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
 public class Duel {
 
-    public static boolean fight(Warrior warriorOne, Warrior warriorTwo){
+    public static boolean fight(Warrior warriorOne, Warrior warriorTwo) {
 
-        while (warriorOne.isAlive(warriorOne.getHealth()) && warriorTwo.isAlive(warriorTwo.getHealth())){
+        while (warriorOne.isAlive(warriorOne.getHealth()) && warriorTwo.isAlive(warriorTwo.getHealth())) {
             warriorOne.hit(warriorTwo);
-            if (warriorTwo.getHealth() < 0){
+            if (warriorTwo.getHealth() <= 0) {
                 break;
             }
             warriorTwo.hit(warriorOne);
-
         }
         return warriorOne.isAlive(warriorOne.getHealth());
     }
 
-    public Warrior chooseRandomFighterForFirstHit(Warrior warrior, Knight knight){
+    public Warrior chooseRandomFighterForFirstHit(Warrior warrior, Knight knight) {
         List<Warrior> listOfWarriors = Arrays.asList(warrior, knight);
         return listOfWarriors.get(new Random().nextInt(listOfWarriors.size()));
     }
