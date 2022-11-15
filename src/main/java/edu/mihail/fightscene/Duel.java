05ecce9b32995255217beef9828eeb4f62307fbd
mishoto <mihail.dev.io@gmail.com>
2022-11-15 10:34:35 +0200

    refactor Duel to implement Actions

--- src/main/java/edu/mihail/fightscene/Duel.java
+++ src/main/java/edu/mihail/fightscene/Duel.java
@@ -1,15 +1,10 @@
 package edu.mihail.fightscene;
 
-import edu.mihail.characters.Knight;
 import edu.mihail.characters.Warrior;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Random;
+public class Duel implements Actions<Warrior> {
 
-public class Duel {
-
-    public static boolean fight(Warrior warriorOne, Warrior warriorTwo) {
+    public boolean fight(Warrior warriorOne, Warrior warriorTwo) {
 
         while (warriorOne.isAlive(warriorOne.getHealth()) && warriorTwo.isAlive(warriorTwo.getHealth())) {
             warriorOne.hit(warriorTwo);
@@ -19,10 +14,5 @@ public class Duel {
             warriorTwo.hit(warriorOne);
         }
         return warriorOne.isAlive(warriorOne.getHealth());
-    }
-
-    public Warrior chooseRandomFighterForFirstHit(Warrior warrior, Knight knight) {
-        List<Warrior> listOfWarriors = Arrays.asList(warrior, knight);
-        return listOfWarriors.get(new Random().nextInt(listOfWarriors.size()));
     }
 }
