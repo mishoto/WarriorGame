5367f238b5452671ab3ff14f839c16fa1ffd79f3
mishoto <mihail.dev.io@gmail.com>
2022-11-16 15:11:06 +0200

    refactor fight with new static method

--- src/main/java/edu/mihail/fightscene/Duel.java
+++ src/main/java/edu/mihail/fightscene/Duel.java
@@ -2,18 +2,21 @@ package edu.mihail.fightscene;
 
 import edu.mihail.models.Warrior;
 
-public class Duel implements Actions<Warrior> {
+public class Duel implements FightStrategy<Warrior>{
 
     @Override
     public boolean fight(Warrior warriorOne, Warrior warriorTwo) {
+        twoWarriorsFightToDeath(warriorOne, warriorTwo);
+        return warriorOne.isAlive();
+    }
 
-        while (warriorOne.isAlive(warriorOne.getHealth()) && warriorTwo.isAlive(warriorTwo.getHealth())) {
+    public static void twoWarriorsFightToDeath(Warrior warriorOne, Warrior warriorTwo) {
+        while (warriorOne.isAlive() && warriorTwo.isAlive()) {
             warriorOne.hit(warriorTwo);
             if (warriorTwo.getHealth() <= 0) {
                 break;
             }
             warriorTwo.hit(warriorOne);
         }
-        return warriorOne.isAlive(warriorOne.getHealth());
     }
 }
