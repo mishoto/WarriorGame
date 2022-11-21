4bd2113bf66a8873cdcb7ceb2038d2f8c3cfbeca
mishoto <mihail.dev.io@gmail.com>
2022-11-21 10:36:47 +0200

    minor changes

--- src/main/java/edu/mihail/models/Defender.java
+++ src/main/java/edu/mihail/models/Defender.java
@@ -3,18 +3,14 @@ package edu.mihail.models;
 public class Defender extends Warrior {
 
     private static final int ATTACK = 3;
-    private static final int DEFENCE = 2;
 
     public Defender() {
         setHealth(60);
+        setDefence(2);
     }
 
     @Override
     public int getAttack() {
         return ATTACK;
-    }
-
-    public int getDefence() {
-        return DEFENCE;
     }
 }
