3e3491151f83845164b5c4066adb33d869a8faae
mishoto <mihail.dev.io@gmail.com>
2022-11-21 10:36:18 +0200

    refactor hit() add defence field add logic for: calculateWarriorHealth() calculateWarriorDamage()

--- src/main/java/edu/mihail/models/Warrior.java
+++ src/main/java/edu/mihail/models/Warrior.java
@@ -1,19 +1,51 @@
 package edu.mihail.models;
 
+import edu.mihail.factories.CharacterType;
+
 public class Warrior extends Character implements Cloneable {
 
     private static final int ATTACK = 5;
+    protected int defence = 0;
 
     public Warrior() {
         setHealth(50);
+        setDefence(0);
     }
 
     public void hit(Warrior warrior) {
-        if(warrior instanceof Defender && ((Defender) warrior).getDefence() < this.getAttack()){
-            warrior.health = warrior.getHealth() + ((Defender) warrior).getDefence() - this.getAttack();
-        }else{
-            warrior.health = warrior.getHealth() - this.getAttack();
+        warrior.health = calculateWarriorHealth(getTypeFromWarriorInstance(warrior), warrior);
+    }
+
+    public void hit(Warrior warriorOne, Warrior warriorTwo) {
+        this.hit(warriorOne);
+        this.hit(warriorTwo);
+    }
+
+    protected int calculateWarriorHealth(CharacterType characterType, Warrior attackedWarrior) {
+        int attackedWarriorHealth = attackedWarrior.getHealth() - calculateWarriorDamage(attackedWarrior);
+
+        switch (characterType) {
+            case WARRIOR:
+            case KNIGHT:
+            case DEFENDER:
+            case VAMPIRE:
+            case LANCER:
+                return attackedWarriorHealth;
+            default:
+                throw new IllegalArgumentException("Wrong Character Type");
         }
+    }
+
+    protected int calculateWarriorDamage(Warrior attackedWarrior) {
+        int attackedWarriorDefence = attackedWarrior.getDefence();
+        int attackerAttack = this.getAttack();
+
+        return attackedWarriorDefence < attackerAttack ? attackerAttack - attackedWarriorDefence : attackerAttack;
+    }
+
+    public static CharacterType getTypeFromWarriorInstance(Warrior warrior) {
+        String packageName = warrior.getClass().getPackageName();
+        return CharacterType.valueOf(warrior.getClass().getTypeName().substring(packageName.length() + 1).toUpperCase());
     }
 
     @Override
@@ -27,13 +59,16 @@ public class Warrior extends Character implements Cloneable {
         }
     }
 
-    @Override
-    public int getHealth() {
-        return this.health;
-    }
-
     public int getAttack() {
         return ATTACK;
+    }
+
+    public int getDefence() {
+        return defence;
+    }
+
+    protected void setDefence(int defence) {
+        this.defence = defence;
     }
 
     @Override
