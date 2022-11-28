b4cc7683c5b2adae2414f626abb3a60143c6e833
mishoto <mihail.dev.io@gmail.com>
2022-11-28 07:42:57 +0200

    add constants for weapons

--- src/main/java/edu/mihail/utils/Constants.java
+++ src/main/java/edu/mihail/utils/Constants.java
@@ -56,4 +56,29 @@ public final class Constants {
         public static final int INITIAL_HEALTH = 60;
         public static final int HEALTH_POINTS = 2;
     }
+
+    public static final class Weapon {
+        private Weapon(){}
+        public static final int SWORD_HEALTH = 5;
+        public static final int SWORD_ATTACK = 2;
+
+        public static final int SHIELD_HEALTH = 20;
+        public static final int SHIELD_ATTACK = -1;
+        public static final int SHIELD_DEFENCE = 2;
+
+        public static final int GREAT_AXE_HEALTH = -15;
+        public static final int GREAT_AXE_ATTACK = 5;
+        public static final int GREAT_AXE_DEFENCE = -2;
+        public static final int GREAT_AXE_VAMPIRISM = 10;
+
+        public static final int KATANA_HEALTH = -20;
+        public static final int KATANA_ATTACK = 6;
+        public static final int KATANA_DEFENCE = -5;
+        public static final int KATANA_VAMPIRISM = 50;
+
+        public static final int MAGIC_WAND_HEALTH = 30;
+        public static final int MAGIC_WAND_ATTACK = 3;
+        public static final int MAGIC_WAND_HEAL_POWER = 3;
+
+    }
 }
