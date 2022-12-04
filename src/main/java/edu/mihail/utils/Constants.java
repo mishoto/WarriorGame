97a530c24e640a6373dd1a98f0470de955935526
mishoto <mihail.dev.io@gmail.com>
2022-12-04 22:22:39 +0200

    minor changes in Constants

--- src/main/java/edu/mihail/utils/Constants.java
+++ src/main/java/edu/mihail/utils/Constants.java
@@ -1,3 +1,4 @@
+
 package edu.mihail.utils;
 
 public final class Constants {
@@ -11,7 +12,6 @@ public final class Constants {
 
         public static final int ATTACK = 5;
         public static final int INITIAL_HEALTH = 50;
-        public static final int DEFENCE = 0;
     }
 
     public static final class Knight {
@@ -20,7 +20,6 @@ public final class Constants {
 
         public static final int ATTACK = 7;
         public static final int INITIAL_HEALTH = 50;
-        public static final int DEFENCE = 0;
     }
 
     public static final class Defender {
@@ -57,28 +56,46 @@ public final class Constants {
         public static final int HEALTH_POINTS = 2;
     }
 
+    public static final class Warlord {
+        private Warlord(){}
+        public static final int ATTACK = 4;
+        public static final int INITIAL_HEALTH = 100;
+        public static final int DEFENCE = 2;
+    }
+
     public static final class Weapon {
         private Weapon(){}
         public static final int SWORD_HEALTH = 5;
         public static final int SWORD_ATTACK = 2;
+        public static final int SWORD_DEFENCE = 0;
+        public static final int SWORD_VAMPIRISM = 0;
+        public static final int SWORD_HEAL_POWER = 0;
 
         public static final int SHIELD_HEALTH = 20;
         public static final int SHIELD_ATTACK = -1;
         public static final int SHIELD_DEFENCE = 2;
+        public static final int SHIELD_VAMPIRISM = 0;
+        public static final int SHIELD_HEAL_POWER = 0;
 
         public static final int GREAT_AXE_HEALTH = -15;
         public static final int GREAT_AXE_ATTACK = 5;
         public static final int GREAT_AXE_DEFENCE = -2;
         public static final int GREAT_AXE_VAMPIRISM = 10;
+        public static final int GREAT_AXE_HEAL_POWER = 0;
 
         public static final int KATANA_HEALTH = -20;
         public static final int KATANA_ATTACK = 6;
         public static final int KATANA_DEFENCE = -5;
         public static final int KATANA_VAMPIRISM = 50;
+        public static final int KATANA_HEAL_POWER = 0;
 
         public static final int MAGIC_WAND_HEALTH = 30;
         public static final int MAGIC_WAND_ATTACK = 3;
+        public static final int MAGIC_WAND_DEFENCE = 0;
+        public static final int MAGIC_WAND_VAMPIRISM = 0;
         public static final int MAGIC_WAND_HEAL_POWER = 3;
 
     }
 }
+
+
