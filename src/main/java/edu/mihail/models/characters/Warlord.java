e357b7087f84bb8b9bdea96bed795478dc1d64f7
mishoto <mihail.dev.io@gmail.com>
2022-12-04 22:38:41 +0200

    create Warlord

---
+++ src/main/java/edu/mihail/models/characters/Warlord.java
@@ -0,0 +1,30 @@
+package edu.mihail.models.characters;
+
+import edu.mihail.models.contracts.AbstractWarrior;
+import edu.mihail.utils.Constants;
+
+public class Warlord extends AbstractWarrior {
+
+    public Warlord() {
+        setHealth();
+    }
+
+    @Override
+    public int getAttack() {
+        return Constants.Warlord.ATTACK;
+    }
+
+    @Override
+    public int getHealth() {
+        return this.health;
+    }
+
+    public int getDefence(){
+        return Constants.Warlord.DEFENCE;
+    }
+
+    private void setHealth(){
+        this.health = Constants.Warlord.INITIAL_HEALTH;
+    }
+
+}
