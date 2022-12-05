e9628b13553dacf22087bd6edb7951de4ca1a3a4
mishoto <mihail.dev.io@gmail.com>
2022-12-05 14:09:12 +0200

    add some logic to test communication channels

--- src/test/java/edu/test/services/observerservice/ObserverServiceTest.java
+++ src/test/java/edu/test/services/observerservice/ObserverServiceTest.java
@@ -4,11 +4,12 @@ import edu.mihail.factories.character.CharacterType;
 import edu.mihail.models.aggregates.Army;
 import edu.mihail.models.contracts.AbstractWarrior;
 import edu.mihail.models.contracts.Character;
+import edu.mihail.services.observerservice.ArmyArrangeListener;
 import edu.mihail.services.observerservice.EventType;
 import edu.mihail.services.observerservice.HealListener;
+import edu.mihail.services.observerservice.RemoveDeadListener;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
 
 class ObserverServiceTest {
 
@@ -23,14 +24,21 @@ class ObserverServiceTest {
 
 
     @Test
-            void shouldTestArmyNotificationService(){
+            void shouldTestNotificationService(){
         {
-            List<Character> armyList = army.getTroop();
-            AbstractWarrior abstractWarrior = (AbstractWarrior) armyList.get(0);
-            armyList.get(0).getNotificationService().subscribe(EventType.HEAL, new HealListener());
+            Character firstCharacter = army.getTroop().get(0);
+            Character secondCharacter = army.getTroop().get(1);
+            Character lastCharacter = army.getTroop().get(army.getTroop().size()-1);
 
-            abstractWarrior.healFirstWarrior();
+            secondCharacter.getNotificationService().subscribe(EventType.HEAL, new HealListener());
+            lastCharacter.getNotificationService().subscribe(EventType.ARMY_ARRANGE, new ArmyArrangeListener());
+            army.getArmyNotificationService().subscribe(EventType.REMOVE_DEAD, new RemoveDeadListener());
+
+            firstCharacter.getNotificationService().notifyCharacter(EventType.HEAL, secondCharacter);
+            firstCharacter.getNotificationService().notifyCharacter(EventType.ARMY_ARRANGE, lastCharacter);
+            lastCharacter.getNotificationService().notifyArmy(EventType.REMOVE_DEAD, army);
+
+
         }
     }
-
 }
